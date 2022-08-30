1ca320ede7a1db2e110e19b734310dcf84671e6e
Bryan Ralphs <38300718+bjralphs@users.noreply.github.com>
2022-08-30 12:51:50 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//"I declare that the following source code was written solely by me.
+//I understand that copying any source code, in whole or in part, constitutes cheating,
+//and that I will receive a zero on this project if I am found in violation of this policy.”
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+string item = Directory.GetCurrentDirectory();
+string path = item + @"\" + "pictures";
+
+//Create new folder to hold rotated pictures
+string new_path = item + @"\" + "rotated_pictures";
+if (Directory.Exists(new_path) == false)
+{
+    Directory.CreateDirectory(new_path);
+}
+
+//Initialize Bitmap/image
+Bitmap bitmap1;
+
+//FUNCTION
+//Accepts image path, rotate then save to new directory
+if (Directory.Exists(path))
+{
+    List<Bitmap> fruits = new List<Bitmap>();
+    foreach (string fileName in Directory.GetFiles(path))
+    {
+        bitmap1 = (Bitmap)Bitmap.FromFile(fileName);
+        fruits.Add(bitmap1);
+    }
+    //BEGIN STOPWATCH
+    var watch = Stopwatch.StartNew();
+        Parallel.ForEach(fruits, i =>
+        {
+            i.RotateFlip(RotateFlipType.Rotate270FlipY);
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Thread " + threadID);
+        }
+        );
+        watch.Stop();
+        TimeSpan ts = watch.Elapsed;
+        Console.WriteLine("Total time elapsed: " + ts);
+    for (int i = 0; i < fruits.Count; i++)
+    {
+        Bitmap fileName = fruits[i];
+        string fileSavePath = Path.Combine(new_path, "image_rotated_" + i);
+        Image copy = fileName;
+        copy.Save(fileSavePath, ImageFormat.Jpeg);
+   
+    }
+}
+else
+{
+    Console.WriteLine("{0} is not a valid file or directory.", path);
+}
