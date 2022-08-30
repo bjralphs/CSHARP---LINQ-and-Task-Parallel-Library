123a8fb6022a365e9ad8a4edccb2c3d40b4cf34e
Bryan Ralphs <38300718+bjralphs@users.noreply.github.com>
2022-08-30 12:52:30 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
-﻿//"I declare that the following source code was written solely by me.
-//I understand that copying any source code, in whole or in part, constitutes cheating,
-//and that I will receive a zero on this project if I am found in violation of this policy.”
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
